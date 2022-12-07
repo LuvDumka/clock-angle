ad4eb71db0829852c2032a601624060ff1c0110f
Sonu kumar <118708266+bhairava009@users.noreply.github.com>
2022-12-07 10:20:24 +0530

    Add files via upload

---
+++ clockangle.py
@@ -0,0 +1,12 @@
+def findAngle(hh,mm):
+    hh = hh % 12
+    h = (hh*360)//12 + (mm*360)//(12*60)
+    m = (mm*360)//(60)
+    angle = abs(h-m)
+    if angle > 180:
+        angle = 360 - angle
+    return angle
+if __name__ == '__main__':
+    hh = int(input("Enter Hour = "))
+    mm = int(input("Enter Minute = "))
+    print(findAngle(hh,mm))
